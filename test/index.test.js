6ab7cf6d5b7aa0e062b3d4c64c6705d16d0e77bb
aloisbarreras <alois@vicino.io>
2016-03-03 14:26:45 -0500

    added tests for completed order and initialize

--- test/index.test.js
+++ test/index.test.js
@@ -4,145 +4,127 @@ var sandbox = require('clear-env');
 var tester = require('analytics.js-integration-tester');
 var Criteo = require('../lib');
 
-describe('Criteo', function(){
-  var criteo;
-  var analytics;
-  var options = {
-    accountId: '12345'
-  };
+describe('Criteo', function() {
+    var criteo;
+    var analytics;
+    var options = {
+        accountId: '12345'
+    };
 
-  beforeEach(function(){
-    analytics = new Analytics;
-    criteo = new Criteo(options);
-    analytics.use(Criteo);
-    analytics.use(tester);
-    analytics.add(criteo);
-  });
-
-  afterEach(function(){
-    analytics.restore();
-    analytics.reset();
-    criteo.reset();
-    sandbox();
-  });
-
-  it('should have the right settings', function(){
-    analytics.compare(Criteo, integration('Criteo')
-      // .assumesPageview()
-      .option('accountId', ''));
-  });
-
-  describe('before loading', function(){
-    beforeEach(function(){
-      analytics.stub(criteo, 'load');
+    beforeEach(function() {
+        analytics = new Analytics;
+        criteo = new Criteo(options);
+        analytics.use(Criteo);
+        analytics.use(tester);
+        analytics.add(criteo);
     });
 
-    afterEach(function(){
-      criteo.reset();
+    afterEach(function() {
+        analytics.restore();
+        analytics.reset();
+        criteo.reset();
+        sandbox();
     });
 
-    describe('#initialize', function(){
-      // TODO: test .initialize();
+    it('should have the right settings', function() {
+        analytics.compare(Criteo, integration('Criteo')
+            .assumesPageview()
+            .option('accountId', ''));
     });
 
-    describe('should call #load', function(){
-      // TODO: test that .initialize() calls `.load()`
-      // you can remove this if it doesn't call `.load()`.
-    });
-  });
-
-  describe('loading', function(){
-    it('should load', function(done){
-      analytics.load(criteo, done);
-    });
-  });
-
-  describe('after loading', function(){
-    beforeEach(function(done){
-      analytics.once('ready', done);
-      analytics.initialize();
-      // analytics.page();
-    });
-
-
-    describe('#identify', function(){
-      // beforeEach(function(){
-        // TODO: stub the integration global api.
-        // For example:
-        // analytics.stub(window.api, 'identify');
-      // });
-
-      it('should send an id', function(){
-        analytics.identify('id');
-        // TODO: assert that the id is sent.
-        // analytics.called(window.api.identify, 'id');
-      });
-
-      it('should send traits', function(){
-        analytics.identify({ trait: true });
-        // TODO: assert that the traits are sent.
-        // analytics.called(window.api.identify, { trait: true });
-      });
-
-      it('should send an id and traits', function(){
-        analytics.identify('id', { trait: true });
-        // TODO: assert that the id and traits are sent.
-        // analytics.called(window.api.identify, 'id');
-        // analytics.called(window.api.identify, { id: 'id', trait: true });
-      });
-    });
-
-
-    describe('#track', function(){
-      beforeEach(function(){
-        analytics.stub(window.criteo_q, 'push');
-      });
-
-      it('should send an event', function(){
-        // analytics.track('event');
-        // TODO: assert that the event is sent.
-        // analytics.called(window.api.logEvent, 'event');
-      });
-
-      it('should send an event and properties', function(){
-        // analytics.track('event', { property: true });
-        // TODO: assert that the event is sent.
-        // analytics.called(window.api.logEvent, 'event', { property: true });
-      });
-
-      it('should call viewed product', function(){
-        analytics.stub(criteo, 'viewedProduct');
-        analytics.track('Viewed Product', {});
-        analytics.called(criteo.viewedProduct);
-      });
-
-      it('should push events', function(){
-        analytics.track('Viewed Product', {
-          id: 'xyz'
+    describe('before loading', function() {
+        beforeEach(function() {
+            analytics.stub(criteo, 'load');
         });
-        analytics.called(window.criteo_q.push, {
-          event: 'setAccount',
-          account: '12345'
-        }, {
-          event: 'viewItem',
-          item: 'xyz'
-        });
-      });
 
-      it('should push with email', function() {
-        analytics.identify('99999', { email: 'schnie@astronomer.io' });
-        analytics.track('Viewed Product', { id: 'xxxxx' });
-        analytics.called(window.criteo_q.push, {
-          event: 'setAccount',
-          account: '12345'
-        }, {
-          event: 'setEmail',
-          email: 'schnie@astronomer.io'
-        }, {
-          event: 'viewItem',
-          item: 'xxxxx'
+        afterEach(function() {
+            criteo.reset();
         });
-      });
+
+        describe('#initialize', function() {
+            it('should call #load', function() {
+                analytics.initialize(); // ask greg about why this fails
+                analytics.page();
+                analytics.called(criteo.load);
+            });
+
+            it('should call push', function() {
+                window.criteo_q = [];
+                analytics.stub(window.criteo_q, 'push');
+                analytics.initialize();
+                analytics.page();
+                analytics.called(window.criteo_q.push);
+            });
+        });
     });
-  });
+
+    describe('loading', function() {
+        it('should load', function(done) {
+            analytics.load(criteo, done);
+        });
+    });
+
+    describe('after loading', function() {
+        beforeEach(function(done) {
+            analytics.once('ready', done);
+            analytics.initialize();
+            analytics.page();
+        });
+
+        describe('#track', function() {
+            beforeEach(function() {
+                analytics.stub(window.criteo_q, 'push');
+            });
+
+            it('should call viewed product', function() {
+                analytics.stub(criteo, 'viewedProduct');
+                analytics.track('Viewed Product', {});
+                analytics.called(criteo.viewedProduct);
+            });
+
+            it('should call completed order', function() {
+                analytics.stub(criteo, 'completedOrder');
+                analytics.track('Completed Order', {});
+                analytics.called(criteo.completedOrder);
+            });
+
+            it('should push events', function() {
+                analytics.track('Viewed Product', {
+                    id: 'xyz'
+                });
+                analytics.called(window.criteo_q.push, {
+                    event: 'setAccount',
+                    account: '12345'
+                }, {
+                    "event": "setSiteType",
+                    "type": "d"
+                }, {
+                    event: 'viewItem',
+                    item: 'xyz'
+                });
+            });
+
+            it('should push with email', function() {
+                analytics.identify('99999', {
+                    email: 'schnie@astronomer.io'
+                });
+                analytics.track('Viewed Product', {
+                    id: 'xxxxx'
+                });
+                analytics.called(window.criteo_q.push, {
+                    event: 'setAccount',
+                    account: '12345'
+                }, {
+                    event: 'setEmail',
+                    email: 'schnie@astronomer.io'
+                }, {
+                    "event": "setSiteType",
+                    "type": "d"
+                }, {
+                    event: 'viewItem',
+                    item: 'xxxxx'
+                });
+            });
+        });
+    });
 });
