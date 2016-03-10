6bf4192c1d38c04968676a12055174cc4138112d
aloisbarreras <alois@vicino.io>
2016-03-10 14:56:46 -0500

    added custom conversion events to criteo

--- test/index.test.js
+++ test/index.test.js
@@ -5,14 +5,18 @@ var tester = require('analytics.js-integration-tester');
 var Criteo = require('../lib');
 var md5 = require('../lib/md5.min.js');
 
-describe('Criteo', function() {
+describe('Criteo', function () {
     var criteo;
     var analytics;
     var options = {
-        accountId: '12345'
+        accountId: '12345',
+        conversionEvents: [{
+            key: 'Bid on Item',
+            value: 'trackTransaction'
+        }]
     };
 
-    beforeEach(function() {
+    beforeEach(function () {
         analytics = new Analytics;
         criteo = new Criteo(options);
         analytics.use(Criteo);
@@ -20,36 +24,36 @@ describe('Criteo', function() {
         analytics.add(criteo);
     });
 
-    afterEach(function() {
+    afterEach(function () {
         analytics.restore();
         analytics.reset();
         criteo.reset();
         sandbox();
     });
 
-    it('should have the right settings', function() {
+    it('should have the right settings', function () {
         analytics.compare(Criteo, integration('Criteo')
             .assumesPageview()
             .option('accountId', ''));
     });
 
-    describe('before loading', function() {
-        beforeEach(function() {
+    describe('before loading', function () {
+        beforeEach(function () {
             analytics.stub(criteo, 'load');
         });
 
-        afterEach(function() {
+        afterEach(function () {
             criteo.reset();
         });
 
-        describe('#initialize', function() {
-            it('should call #load', function() {
+        describe('#initialize', function () {
+            it('should call #load', function () {
                 analytics.initialize(); // ask greg about why this fails
                 analytics.page();
                 analytics.called(criteo.load);
             });
 
-            it('should call push', function() {
+            it('should call push', function () {
                 window.criteo_q = [];
                 analytics.stub(window.criteo_q, 'push');
                 analytics.initialize();
@@ -59,37 +63,70 @@ describe('Criteo', function() {
         });
     });
 
-    describe('loading', function() {
-        it('should load', function(done) {
+    describe('loading', function () {
+        it('should load', function (done) {
             analytics.load(criteo, done);
         });
     });
 
-    describe('after loading', function() {
-        beforeEach(function(done) {
+    describe('after loading', function () {
+        beforeEach(function (done) {
             analytics.once('ready', done);
             analytics.initialize();
             analytics.page();
         });
 
-        describe('#track', function() {
-            beforeEach(function() {
+        describe('#track', function () {
+            beforeEach(function () {
                 analytics.stub(window.criteo_q, 'push');
             });
 
-            it('should call viewed product', function() {
+            it('should call track', function () {
+                analytics.stub(criteo, 'track');
+                analytics.track('My Event', {});
+                analytics.called(criteo.track);
+            });
+
+            it('should call viewed product', function () {
                 analytics.stub(criteo, 'viewedProduct');
                 analytics.track('Viewed Product', {});
                 analytics.called(criteo.viewedProduct);
             });
 
-            it('should call completed order', function() {
+            it('should call completed order', function () {
                 analytics.stub(criteo, 'completedOrder');
                 analytics.track('Completed Order', {});
                 analytics.called(criteo.completedOrder);
             });
 
-            it('should push events', function() {
+            it('should call custom conversion events', function () {
+                analytics.track('Bid on Item', {
+                    id: "My Order Id",
+                    products: [{
+                        id: "My Item Id",
+                        price: 100,
+                        quantity: 1
+                    }]
+                });
+
+                analytics.called(window.criteo_q.push, {
+                    event: 'setAccount',
+                    account: '12345'
+                }, {
+                    "event": "setSiteType",
+                    "type": "d"
+                }, {
+                    event: "trackTransaction",
+                    id: 'My Order Id',
+                    item: [{
+                        id: "My Item Id",
+                        price: 100,
+                        quantity: 1
+                    }]
+                });
+            });
+
+            it('should push events', function () {
                 analytics.track('Viewed Product', {
                     id: 'xyz'
                 });
@@ -105,7 +142,7 @@ describe('Criteo', function() {
                 });
             });
 
-            it('should push with email', function() {
+            it('should push with email', function () {
                 analytics.identify('99999', {
                     email: 'schnie@astronomer.io'
                 });
